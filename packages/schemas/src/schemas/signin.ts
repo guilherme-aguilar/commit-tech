6e483b0d4ea43918bf80a3733b1c25aa0afabd1b
Gui.Goncalves <gui.aguiar1945@gmail.com>
2025-05-14 16:43:45 -0300

    feat: create schematic packages in packages/ directory

---
+++ packages/schemas/src/schemas/signin.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+
+export const SigninDTOSchema  = z.object({
+  email: z.string().min(1, { message: "Email é obrigatório" }).nonempty(),
+  password: z.string().min(1, { message: "Senha é obrigatória" }).nonempty(),
+}).strict();
