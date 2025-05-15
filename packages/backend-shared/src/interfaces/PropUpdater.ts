8309e04c5df8f18df7ed8632fc69926e05125374
GuiAguiar <159579163+guilherme-aguilar@users.noreply.github.com>
2025-05-15 00:04:32 -0300

    Revert "Packages/backend shared"

--- packages/backend-shared/src/interfaces/PropUpdater.ts
+++
@@ -1,4 +0,0 @@
-export interface PropUpdater {
-  canHandle(value: unknown, target?: any, key?: string): boolean;
-  update(target: any, key: string, value: unknown): void;
-}
--- packages/backend-shared/src/utils/isPlainObject.ts
+++
@@ -1,2 +0,0 @@
-export const isPlainObject = (x: unknown): x is object =>
-  typeof x === 'object' && x !== null && !Array.isArray(x);
