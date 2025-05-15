e2d32f6e9da30aad8dea20616b1e7dffaa1a0397
Gui.Goncalves <gui.aguiar1945@gmail.com>
2025-05-15 01:07:17 -0300

    feat(backend): create UserEntity and add backend dev script

--- apps/backend/src/domain/entities/user.entity.ts
+++ apps/backend/src/domain/entities/user.entity.ts
@@ -1,8 +1,18 @@
-import { BasicModel, ContactInfoModel, AddressModel} from "@commit-tech/system-shared";
+import { BasicModel, ContactInfoModel, AddressModel, BasicEntity} from "@commit-tech/system-shared";
 
 export interface UserModel extends BasicModel {
   firstName: string;
   lastName: string;
   contactInfo: ContactInfoModel;
   address: AddressModel;
+}
+
+type model = UserModel;
+
+export class UserEntity extends BasicEntity<model> {
+  constructor(
+    props: model, 
+    id?: string) {
+    super(props, id);
+  }
 }
